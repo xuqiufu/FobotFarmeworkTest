73ba0c6bb66a9292df04b23798231ef41007236c
unknown <xuqiufu@company.local>
2022-08-04 15:12:32 +0800

    linux_jenkinsfile

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -5,13 +5,32 @@ pipeline {
     stages {  
         stage("build") {
             steps {
-		  script{
-		    sh"""
-		    ${build_commnd}
-		    """	  	    		  				
-		    echo " ${build_commnd}"
-		  }
+				script{
+					sh"""
+					${build_commnd}
+					"""	  	    		  				
+					echo " ${build_commnd}"
+					}
+				}
 			}
 		}
-	}	
+		post {
+			always { 
+				script{
+					step(
+							[
+                              $class              : 'RobotPublisher',
+                              outputPath          : '.',
+                              outputFileName      : 'output.xml',
+                              reportFileName      : 'report.html',
+                              logFileName         : 'log.html',
+                              disableArchiveOutput: false,
+                              passThreshold       : 100,
+                              unstableThreshold   : 80,
+                            ]
+						)
+					echo "The report have update!"
+					}
+				}
+			}	
 }
